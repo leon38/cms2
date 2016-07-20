4f5a6309b5c2cdb49407fd59ff68ef0142b48743
Damien Corona <dcorona@3c-e.com>
2016-07-20 17:25:30 +0200

    template category

--- src/CMS/Bundle/FrontBundle/Controller/FrontController.php
+++ src/CMS/Bundle/FrontBundle/Controller/FrontController.php
@@ -77,7 +77,7 @@ class FrontController extends Controller
 				}
 				throw new NotFoundHttpException("Page not found");
 			}
-      $parameters = array_merge($this->_parameters,array('title' => $category->getTitle(), 'theme' => $this->_theme, 'contents' => $category->getContents()));
+      $parameters = array_merge($this->_parameters,array('title' => $category->getTitle(), 'category' => $category,'theme' => $this->_theme, 'contents' => $category->getContents()));
       return $this->render('@cms/'.$this->_theme.'/category.html.twig', $parameters);
 		}
     $parameters = array_merge($this->_parameters,array('title' => $content->getTitle(), 'theme' => $this->_theme, 'content' => $content));
