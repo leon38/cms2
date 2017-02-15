762376536260c517ee63411ce8775459131e3a8c
Damien Corona <dcorona@3c-e.com>
2017-02-15 17:15:09 +0100

    sitemap google

--- src/CMS/Bundle/CoreBundle/Classes/Widgets/Share.php
+++ src/CMS/Bundle/CoreBundle/Classes/Widgets/Share.php
@@ -17,9 +17,33 @@ class Share extends Widget
     public function display($param = array())
     {
         $params = array_merge($this->getParams(), $param);
+        $params['bitlyUrl'] = $this->makeBitlyUrl('https://choupdoune.fr/'.$params['content']->getUrl().'.html');
         return $this->getTemplating()->render(
             'CoreBundle:Widget:share.html.twig',
             $params
         );
+    }
+
+
+    private function makeBitlyUrl($url, $login = 'o_58am3tc0eh', $appkey = 'R_a7af71df000d4233911d0b1983084953',$format = 'xml', $version = '2.0.1')
+    {
+        //create the URL
+        $bitly = 'http://api.bit.ly/shorten?version='.$version.'&longUrl='.urlencode($url).'&login='.$login.'&apiKey='.$appkey.'&format='.$format;
+
+        //get the url
+        //could also use cURL here
+        $response = file_get_contents($bitly);
+
+        //parse depending on desired format
+        if(strtolower($format) == 'json')
+        {
+            $json = @json_decode($response,true);
+            return $json['results'][$url]['shortUrl'];
+        }
+        else //xml
+        {
+            $xml = simplexml_load_string($response);
+            return 'http://bit.ly/'.$xml->results->nodeKeyVal->hash;
+        }
     }
 }
---
+++ src/CMS/Bundle/FrontBundle/Controller/SitemapController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace CMS\Bundle\FrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class SitemapController extends Controller
+{
+    /**
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/sitemap.{_format}", name="sitemap", requirements={"_format": "xml"})
+     */
+    public function indexAction()
+    {
+        $query = $this->getDoctrine()->getRepository('ContentBundle:Content')->getAllContentsNotTrashedQuery();
+        $contents = $query->getResult();
+        $categories = $this->getDoctrine()->getRepository('ContentBundle:Category')->findBy(array('published' => 1));
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/xml; charset=utf-8');
+        return $this->render('FrontBundle:Sitemap:sitemap.xml.twig', array('contents' => $contents, 'categories' => $categories), $response);
+    }
+}
