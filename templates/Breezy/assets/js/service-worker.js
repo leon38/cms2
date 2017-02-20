305ad070ec847317554b76abbf70750a59448121
Damien Corona <dcorona@3c-e.com>
2017-02-20 15:10:36 +0100

    service worker

--- templates/Breezy/assets/js/service-worker.js
+++ templates/Breezy/assets/js/service-worker.js
@@ -13,14 +13,27 @@ self.addEventListener('install', e => {
     )
 });
 
-self.addEventListener('activate',  event => {
-    event.waitUntil(self.clients.claim());
+addEventListener('activate', event => {
+    event.waitUntil(async function() {
+        // Feature-detect
+        if (self.registration.navigationPreload) {
+            // Enable navigation preloads!
+            await self.registration.navigationPreload.enable();
+        }
+    }());
 });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    )
+addEventListener('fetch', event => {
+    event.respondWith(async function() {
+        // Respond from the cache if we can
+        const cachedResponse = await caches.match(event.request);
+        if (cachedResponse) return cachedResponse;
+
+        // Else, use the preloaded response, if it's there
+        const response = await event.preloadResponse;
+        if (response) return response;
+
+        // Else try the network.
+        return fetch(event.request);
+    }());
 });
